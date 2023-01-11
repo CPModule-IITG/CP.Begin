d0d12037c14c5b3d542918a1c24ee858f4341734
Aditya Mandal <72205646+addy-1406@users.noreply.github.com>
2023-01-11 15:37:23 +0530

    Update Binary_Exponentiation.cpp

--- Week_2/Cpp_Functions/Binary_Exponentiation.cpp
+++ Week_2/Cpp_Functions/Binary_Exponentiation.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 typedef long long ll;
 
-// Binary Exponentiation
+/** Binary Exponentiation */
 ll binpow(ll x, ll n) {
     if (n == 0) return 1;
     ll res = binpow(x, n / 2);
@@ -12,7 +12,7 @@ ll binpow(ll x, ll n) {
     return res * res;
 }
 
-// Modular Binary Exponentiation
+/** Modular Binary Exponentiation */
 ll binpow_mod(ll x, ll n, ll m){
     if (n == 0) return 1;
     ll res = binpow_mod(x, n / 2, m);
@@ -21,9 +21,24 @@ ll binpow_mod(ll x, ll n, ll m){
     return (res * res) % m;
 }
 
+/** Computes x^y modulo p in O(log p) time (more efficient than above) */
+ll exp(ll x, ll y, ll p) {
+	ll res = 1; x %= p;
+	while (y) {
+		if (y & 1) {
+			res *= x; res %= p; 
+		}
+		x *= x;
+		x %= p;
+		y >>= 1;
+	}
+	return res;
+}
+
 int main() {
     // driver code
     ll x = 23;
     cout << binpow(x,2) << "\n";
     cout << binpow_mod(x,2,10) << "\n"; 
+    cout << exp(x,2,10) << "\n"; 
 }
