6a190ad0c85a9b40ab95929b5306f41606e2c51e
addy <addy140603@gmail.com>
2023-01-18 16:32:09 +0530

    .

---
+++ Week_3/Cpp/bfs.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+int main() {
+    // Graph representation using adjacency list
+    ll n = 5; // no of vertices
+    ll e = 7; // no of edges
+    // using 1-based indexing thus the vertices are 1,2,3,...n
+    vector<vector<ll>> adj(n+1); // declaring adjacency list
+
+    vector<pair<ll,ll>> edges = {{1,2},{2,3},{1,4},{4,5},{3,4},{3,5},{2,5}};
+
+    for(auto x:edges){
+        ll a = x.first;
+        ll b = x.second;
+        adj[a].push_back(b); // adding vertex b to adjacent list of vertex a
+        adj[b].push_back(a); // adding vertex a to adjacent list of vertex b
+    }
+    
+    // vis will store if the vertex i is visited for index i
+    vector<bool> vis(n+1,false);
+
+    // this will print the sequence of vertices which our dfs functions visits
+    queue<ll> q;
+    q.push(1);
+    vis[1] = true;
+    while(!q.empty()){
+        ll vertex = q.front();
+        cout << vertex << " ";
+        q.pop();
+        for(auto child:adj[vertex]){
+            if(!vis[child]){
+                q.push(child);
+                vis[child] = true;
+            }
+        }
+    }  
+}
---
+++ Week_3/Cpp/dfs.cpp
@@ -0,0 +1,42 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+
+void dfs(ll vertex, vector<vector<ll>> &adj, vector<bool> &vis){
+    if(vis[vertex] == true){
+        return;
+    }
+    vis[vertex] = true;
+    cout << vertex << " ";
+    for(auto child:adj[vertex]){
+        dfs(child, adj, vis);
+    }
+    return;
+}
+
+int main() {
+    // Graph representation using adjacency list
+    ll n = 5; // no of vertices
+    ll e = 7; // no of edges
+    // using 1-based indexing thus the vertices are 1,2,3,...n
+    vector<vector<ll>> adj(n+1); // declaring adjacency list
+
+    vector<pair<ll,ll>> edges = {{1,2},{2,3},{1,4},{4,5},{3,4},{3,5},{2,5}};
+
+    for(auto x:edges){
+        ll a = x.first;
+        ll b = x.second;
+        adj[a].push_back(b); // adding vertex b to adjacent list of vertex a
+        adj[b].push_back(a); // adding vertex a to adjacent list of vertex b
+    }
+    
+    // vis will store if the vertex i is visited for index i
+    vector<bool> vis(n+1,false);
+
+    // this will print the sequence of vertices which our dfs functions visits
+    dfs(1, adj, vis);
+
+
+
+}
